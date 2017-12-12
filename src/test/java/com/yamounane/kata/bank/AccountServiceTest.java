a60eee4cc2439106037d5c2eb33c3c1af6038f19
Yassine Amounane <yassine.am@gmail.com>
2017-12-12 23:31:13 +0100

    ADD deposit operation test

--- src/test/java/com/yamounane/kata/bank/AccountServiceTest.java
+++ src/test/java/com/yamounane/kata/bank/AccountServiceTest.java
@@ -1,8 +1,19 @@
 package com.yamounane.kata.bank;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import com.yamounane.kata.bank.exception.AccountException;
+import com.yamounane.kata.bank.model.Account;
+import com.yamounane.kata.bank.model.Customer;
+import com.yamounane.kata.bank.service.AccountServiceImpl;
 
 /**
  * Test class for {@link AccountService}
@@ -11,9 +22,34 @@ import org.junit.Test;
  */
 public class AccountServiceTest {
 	
+	AccountService service = new AccountServiceImpl();
+	
+	private Customer customerDoe;
+	
+	private Account accountDoe;
+	
+	private final String DOE_ACCOUNT_ID = "A001";
+	
+	private final BigDecimal ONE_THOUSAND = new BigDecimal(1000);
+
+    @Before
+    public void setUp() throws Exception {  
+    	//John Doe customer & account initilization
+    	customerDoe = new Customer("C001", "John", "Doe");
+    	accountDoe = new Account(DOE_ACCOUNT_ID);
+    	List<Account> doeAccounts = new ArrayList<>();
+    	doeAccounts.add(accountDoe);
+    	customerDoe.setAccounts(doeAccounts);
+    }	
+	
 	@Test
 	public void checkDepositOneThousandOperationForDoeAccount() {
-		fail();
+        try {
+			service.deposit(customerDoe, DOE_ACCOUNT_ID, ONE_THOUSAND, "OP001");
+		} catch (AccountException e) {
+			fail();
+		}
+        assertEquals(ONE_THOUSAND, accountDoe.getBalance());
 	}
 	
 	@Test
