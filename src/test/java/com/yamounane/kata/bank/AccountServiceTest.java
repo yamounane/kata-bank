67542b662076be4fd7f5341ecd29bf3498f35d39
Yassine Amounane <yassine.am@gmail.com>
2017-12-12 23:18:02 +0100

    init Account Service Test class

---
+++ src/test/java/com/yamounane/kata/bank/AccountServiceTest.java
@@ -0,0 +1,34 @@
+package com.yamounane.kata.bank;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+/**
+ * Test class for {@link AccountService}
+ *  
+ * @author Yassine Amounane
+ */
+public class AccountServiceTest {
+	
+	@Test
+	public void checkDepositOneThousandOperationForDoeAccount() {
+		fail();
+	}
+	
+	@Test
+	public void checkWithdrawOperationOneThousandForDoeAccount() {
+		fail();
+	}
+	
+	@Test
+	public void checkBalanceForDoeAccountWithFiveHundred() {
+		fail();
+	}
+	
+	@Test
+	public void checkStatementForOneThousandDoeAccount() {
+		fail();
+	}
+
+}
