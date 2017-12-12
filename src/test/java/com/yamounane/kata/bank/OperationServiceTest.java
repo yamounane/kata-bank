ff91b5b920d75df4852ca7c0a1b112bd0edc560b
Yassine Amounane <yassine.am@gmail.com>
2017-12-12 23:47:20 +0100

    init operation service test class

---
+++ src/test/java/com/yamounane/kata/bank/OperationServiceTest.java
@@ -0,0 +1,21 @@
+package com.yamounane.kata.bank;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import com.yamounane.kata.bank.service.OperationService;
+
+/**
+ * Test class for {@link OperationService}
+ *  
+ * @author Yassine Amounane
+ */
+public class OperationServiceTest {
+
+	@Test
+	public void checkAddDepositOperationOfOneThousandForDoeAccount() {
+		fail();
+	}
+	
+}
