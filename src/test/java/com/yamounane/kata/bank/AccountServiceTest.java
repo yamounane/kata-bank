16a01fc8125b03097f23a3bd7edba402c84befea
Yassine Amounane <yassine.am@gmail.com>
2017-12-28 23:21:03 +0100

    refactor account service test class due to refactor on operation service

--- src/test/java/com/yamounane/kata/bank/AccountServiceTest.java
+++ src/test/java/com/yamounane/kata/bank/AccountServiceTest.java
@@ -2,27 +2,37 @@ package com.yamounane.kata.bank;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.any;
 
 import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import com.yamounane.kata.bank.exception.AccountException;
 import com.yamounane.kata.bank.model.Account;
 import com.yamounane.kata.bank.model.Customer;
+import com.yamounane.kata.bank.model.Operation;
+import com.yamounane.kata.bank.model.OperationType;
 import com.yamounane.kata.bank.service.AccountServiceImpl;
 import com.yamounane.kata.bank.service.OperationService;
-import com.yamounane.kata.bank.service.OperationServiceImpl;
 
 /**
  * Test class for {@link AccountService}
  * 
  * @author Yassine Amounane
  */
+@RunWith(MockitoJUnitRunner.class)
 public class AccountServiceTest {
 
-	private OperationService operationServiceMock = new OperationServiceImpl();
+	@Mock
+	private OperationService operationServiceMock;
 	private AccountService service = new AccountServiceImpl(operationServiceMock);
 
 	private Customer johnDoe;
@@ -41,6 +51,9 @@ public class AccountServiceTest {
 
 	@Test
 	public void should_account_balance_increase_when_deposit_occur() throws AccountException {
+		Mockito.when(operationServiceMock.addDepositOperation(any(BigDecimal.class), any(String.class)))
+				.thenReturn(new Operation(UUID.randomUUID().toString(), OperationType.CREDIT, Instant.now(), _1000));
+
 		service.deposit(johnDoe, secondAccount, _1000);
 
 		assertThat(secondAccount.getBalance()).isEqualTo(_1000);
@@ -48,8 +61,10 @@ public class AccountServiceTest {
 
 	@Test
 	public void should_deposit_increase_account_specified_without_impacting_other_accounts() throws AccountException {
+		Mockito.when(operationServiceMock.addDepositOperation(any(BigDecimal.class), any(String.class)))
+				.thenReturn(new Operation(UUID.randomUUID().toString(), OperationType.CREDIT, Instant.now(), _1000));
 		BigDecimal secondAccountBalance = secondAccount.getBalance();
-		
+
 		service.deposit(johnDoe, firstAccount, _1000);
 
 		assertThat(secondAccount.getBalance()).isEqualTo(secondAccountBalance);
@@ -57,6 +72,9 @@ public class AccountServiceTest {
 
 	@Test
 	public void should_withdrawal_be_allowed_even_if_balance_becomes_negative() throws AccountException {
+		Mockito.when(operationServiceMock.addDepositOperation(any(BigDecimal.class), any(String.class)))
+				.thenReturn(new Operation(UUID.randomUUID().toString(), OperationType.DEBIT, Instant.now(), _1000));
+
 		service.withdraw(johnDoe, secondAccount, _1000);
 
 		assertThat(secondAccount.getBalance()).isEqualTo(_1000.negate());
@@ -64,6 +82,9 @@ public class AccountServiceTest {
 
 	@Test
 	public void should_deposit_increase_when_multiple_deposit() throws AccountException {
+		Mockito.when(operationServiceMock.addDepositOperation(any(BigDecimal.class), any(String.class)))
+				.thenReturn(new Operation(UUID.randomUUID().toString(), OperationType.CREDIT, Instant.now(), _1000));
+
 		service.deposit(johnDoe, secondAccount, _1000);
 		service.deposit(johnDoe, secondAccount, _1000);
 		service.deposit(johnDoe, secondAccount, _1000);
@@ -75,6 +96,11 @@ public class AccountServiceTest {
 
 	@Test
 	public void should_statement_not_be_empty_when_printing_it() throws AccountException {
+		Mockito.when(operationServiceMock.addDepositOperation(any(BigDecimal.class), any(String.class)))
+				.thenReturn(new Operation(UUID.randomUUID().toString(), OperationType.CREDIT, Instant.now(), _1000));
+		Mockito.when(operationServiceMock.addDepositOperation(any(BigDecimal.class), any(String.class)))
+				.thenReturn(new Operation(UUID.randomUUID().toString(), OperationType.DEBIT, Instant.now(), _1000));
+
 		service.deposit(johnDoe, secondAccount, _1000);
 		service.deposit(johnDoe, secondAccount, _1000);
 		service.withdraw(johnDoe, secondAccount, _1000);
