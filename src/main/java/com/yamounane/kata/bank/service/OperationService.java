17d59b220a746a09cf33354753a85591e0884031
Yassine Amounane <yassine.am@gmail.com>
2017-12-28 23:17:04 +0100

    refactor feature envy on operation service code

--- src/main/java/com/yamounane/kata/bank/service/OperationService.java
+++ src/main/java/com/yamounane/kata/bank/service/OperationService.java
@@ -3,7 +3,7 @@ package com.yamounane.kata.bank.service;
 import java.math.BigDecimal;
 
 import com.yamounane.kata.bank.exception.AccountException;
-import com.yamounane.kata.bank.model.Account;
+import com.yamounane.kata.bank.model.Operation;
 
 /**
  * Operation Service allowing handle banking operations
@@ -11,38 +11,35 @@ import com.yamounane.kata.bank.model.Account;
  * @author Yassine Amounane
  */
 public interface OperationService {
-	
-	/**
-	 * Add CREDIT operation to given account  
-	 * @param account account to add the CREDIT operation
-	 * @param value value of the operation
-	 * @param operationId id of the operation
-	 * @throws AccountException if account does not exists.
-	 */
-	void addDepositOperation(Account account, BigDecimal value, String operationId) throws AccountException;
-	
-	/**
-	 * Add DEBIT operation to given account  
-	 * @param account account to add the DEBIT operation
-	 * @param value value of the operation
-	 * @param operationId id of the operation
-	 * @throws AccountException if account does not exists.
-	 */
-	void addWithdrawOperation(Account account, BigDecimal value, String operationId) throws AccountException;
 
 	/**
-	 * Get statement for the given account  
-	 * @param account account to get the statement
-	 * @throws AccountException if account does not exists.
-	 * @return statement for the given account
+	 * Create CREDIT operation to given account
+	 * 
+	 * @param account
+	 *            account to add the CREDIT operation
+	 * @param value
+	 *            value of the operation
+	 * @param operationId
+	 *            id of the operation
+	 * @return credit operation created
+	 * @throws AccountException
+	 *             if account does not exists.
 	 */
-	String getStatement(Account account) throws AccountException;
-	
+	Operation addDepositOperation(BigDecimal value, String operationId) throws AccountException;
+
 	/**
-	 * Compute balance from given account operations  
-	 * @param account account to compute balance
-	 * @return computed balance
+	 * Create DEBIT operation to given account
+	 * 
+	 * @param account
+	 *            account to add the DEBIT operation
+	 * @param value
+	 *            value of the operation
+	 * @param operationId
+	 *            id of the operation
+	 * @return debit operation created
+	 * @throws AccountException
+	 *             if account does not exists.
 	 */
-	BigDecimal computeBalance(Account account);
+	Operation addWithdrawOperation(BigDecimal value, String operationId) throws AccountException;
 
 }
--- src/main/java/com/yamounane/kata/bank/service/OperationServiceImpl.java
+++ src/main/java/com/yamounane/kata/bank/service/OperationServiceImpl.java
@@ -2,10 +2,8 @@ package com.yamounane.kata.bank.service;
 
 import java.math.BigDecimal;
 import java.time.Instant;
-import java.util.stream.Collectors;
 
 import com.yamounane.kata.bank.exception.AccountException;
-import com.yamounane.kata.bank.model.Account;
 import com.yamounane.kata.bank.model.Operation;
 import com.yamounane.kata.bank.model.OperationType;
 
@@ -17,45 +15,19 @@ import com.yamounane.kata.bank.model.OperationType;
 public class OperationServiceImpl implements OperationService {
 
 	@Override
-	public void addDepositOperation(Account account, BigDecimal value, String operationId) throws AccountException {
-		if(value.compareTo(BigDecimal.ZERO) != 0) {
-			account.getOperations().add(new Operation(operationId, OperationType.CREDIT, Instant.now(), value.abs()));
-			computeBalance(account);
+	public Operation addDepositOperation(BigDecimal value, String operationId) throws AccountException {
+		if (value.compareTo(BigDecimal.ZERO) != 0) {
+			return new Operation(operationId, OperationType.CREDIT, Instant.now(), value.abs());
 		}
+		return null;
 	}
-	
-	@Override
-	public void addWithdrawOperation(Account account, BigDecimal value, String operationId) throws AccountException {
-		if(value.compareTo(BigDecimal.ZERO) != 0) {
-			account.getOperations().add(new Operation(operationId, OperationType.DEBIT, Instant.now(), value.abs()));
-			computeBalance(account);
-		}
-	}
-	
-	@Override
-	public String getStatement(Account account) throws AccountException {
-		String statement = account.getOperations()
-									.stream()
-										.map(operation -> operation.toString())
-											.collect(Collectors.joining(";"));
 
-		return statement;
-	}
-	
 	@Override
-	public BigDecimal computeBalance(Account account){
-		double debit = getSumOperationFromType(account, OperationType.DEBIT);
-		double credit = getSumOperationFromType(account, OperationType.CREDIT);
-		
-		return new BigDecimal(credit - debit);
+	public Operation addWithdrawOperation(BigDecimal value, String operationId) throws AccountException {
+		if (value.compareTo(BigDecimal.ZERO) != 0) {
+			return new Operation(operationId, OperationType.DEBIT, Instant.now(), value.abs());
+		}
+		return null;
 	}
-	
-	private double getSumOperationFromType(Account account, OperationType type) {
-		return account.getOperations()
-							.stream()
-								.filter(operation -> type.equals(operation.getType()))
-									.mapToDouble(operation -> operation.getAmount().doubleValue())
-										.sum();
-	}
-	
+
 }
